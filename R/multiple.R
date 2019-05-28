eaa261d40244b329c944918ee0bff0f2343c22f4
Asif Tamuri <tamuri@gmail.com>
2019-05-28 14:38:36 +0100

    Hide file input after upload

--- R/multiple.R
+++ R/multiple.R
@@ -6,12 +6,12 @@
     sidebarLayout(
       sidebarPanel(
         h3("Measurements to SDS"),
-        fileInput(ns("file"), "Upload data-file:"),
+
+        uiOutput(ns("fileInput")),
         
-        # Variable selection:
+        # after uploading data, we show:
         uiOutput(ns("sexSelect")),
         uiOutput(ns("ageSelect")),
-        
         uiOutput(ns("measureSelect"))
         ),
       
@@ -28,7 +28,8 @@
 
 # Example server logic ########################################################
 .multiple <- function(input, output, session, stringAsFactors) {
-  ns <- NS('multiple')
+  # To use `renderUI` within modules, we need to wrap with `ns()`
+  ns <- session$ns
   
   rvalues <- reactiveValues(
     df = NULL
@@ -36,6 +37,7 @@
   
   cols <- reactiveVal(NULL)
   
+  # upload data and set dataframe
   observe({
     if(!is.null(input$file)) {
       df <- as.data.frame(read.csv(input$file$datapath))
@@ -44,12 +46,20 @@
     }
   })
   
+  # hide the file input once rvalue has been updated
+  output$fileInput <- renderUI({
+    if(is.null(rvalues$df)) {
+      return(fileInput(ns('file'), 'Upload data file'))
+    } else {
+      return(NULL)
+    }
+  })
+  
   output$table <- renderTable(rvalues$df)
   
   # Select variables:
   output$sexSelect <- renderUI({
-    
-    if (is.null(isolate(rvalues$df)) && is.null(input$file)) return(NULL)
+    if (is.null(rvalues$df)) return(NULL)
     match <- intersect(c('Sex', 'sex', 'Gender', 'gender'), isolate(cols()))
     if (length(match) == 1) {
       selected <- paste('[Column] ', match, sep='')
@@ -67,8 +77,8 @@
   })
   
   output$ageSelect <- renderUI({
-    
-    if (is.null(isolate(rvalues$df)) && is.null(input$file)) return(NULL)
+    # only display age selection if we have a dataframe
+    if (is.null(rvalues$df)) return(NULL)
     match <- intersect(c('Age', 'age', 'Years', 'years', 'Days', 'days'), isolate(cols()))
     
     if (length(match) == 1) {
@@ -90,7 +100,9 @@
   })
   
   output$measureSelect <- renderUI({
-    if (is.null(isolate(rvalues$df)) && is.null(input$file)) return(NULL)
+    # only display measurement selection if we have loaded a dataframe
+    if (is.null(rvalues$df)) return(NULL)
+    
     options <-  c('N/A', paste('[Column] ', isolate(cols()), sep=''))
     output = tagList()
     output[[1]] = selectInput(ns("height"), "Height (cm)", options)
@@ -98,29 +110,61 @@
     output[[3]] = selectInput(ns("bmi"), "BMI (kg/m^2)", options)
     output[[4]] = selectInput(ns("sitht"), "Sitting height (cm)", options)
     output[[5]] = selectInput(ns("legln"), "Leg length (cm)", options)
+    
+    output[[6]] = fluidPage(actionButton(ns('Reset'), 'Reset'), actionButton(ns('Apply'), 'Apply') )
     output
   })
   
-  observe({
-    height_value <- input$height
-    if (!is.null(height_value) && height_value != 'N/A') {
+  get_sex <- function() {
+    sex_selected <- isolate(input$sex)
+    if (startsWith(sex_selected, '[Column]')) {
       df <- isolate(rvalues$df)
-      column = strsplit(height_value, split=" ")[[1]][2]
-      sex_value <- isolate(input$sex)
-      if (startsWith(sex_value, '[Column]')) {
-        sex_column = df[, strsplit(sex_value, split=" ")[[1]][2]]
-      } else {
-        sex_column = sex_value
-      }
-      age_value <- isolate(input$ageSource)
-      age_column = df[, strsplit(age_value, split=" ")[[1]][[2]]]
-      
-      lms_stats <- .measurement_to_scores(age_column, sex_column, 'ht', df[, column])
-      new_column = paste(column, '@Height_SDS', sep='')
-      df[[new_column]] <- lms_stats$z
-      new_order <- append(cols(), new_column, which(cols()==column))
-      rvalues$df <- df[new_order]
+      sex_column_or_value = df[, strsplit(sex_selected, split=" ")[[1]][2]]
+    } else {
+      sex_column_or_value = sex_selected
     }
-  })
+    return(sex_column_or_value)
+  }
+  
+  get_age <- function() {
+      df <- isolate(rvalues$df)
+      age_column_or_value <- isolate(input$ageSource)
+      age_column = df[, strsplit(age_column_or_value, split=" ")[[1]][[2]]]
+      return(age_column)
+  }
+  
+  measurement_names <- list(
+    height=list(name='height', code='ht'),
+    weight=list(name='weight', code='wt')
+  )
+  
+  get_observer <- function(measurement_name) {
+      value <- input[[measurement_name]]
+      input_name <- measurement_names[[measurement_name]]$name
+      code_name <- measurement_names[[measurement_name]]$code
+      
+      if (!is.null(value) && value != 'N/A') {
+        df <- isolate(rvalues$df)
+        column = strsplit(value, split=" ")[[1]][2]
+        sex_column_or_value <- get_sex()
+        age_column_or_value <- get_age()
+        
+        lms_stats <- .measurement_to_scores(age_column_or_value, sex_column_or_value, code_name, df[, column])
+        new_column = paste(column, '@', code_name, '_SDS', sep='')
+        columns <- names(df)
+        df[[new_column]] <- lms_stats$z
+        
+        # fix the column order if it didn't exist already
+        if (is.na(match(new_column, columns))) {
+          new_order <- append(columns, new_column, which(columns==column))  
+          df <- df[new_order]
+        }
+        
+        rvalues$df <- df
+    }
+  }
+  
+  observeEvent(input$height, get_observer('height'))
+  observeEvent(input$weight, get_observer('weight'))
 }
 
