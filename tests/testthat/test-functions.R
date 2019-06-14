5f054419c1d78daffbfe7ab985f390bc8bbc9bae
Asif Tamuri <tamuri@gmail.com>
2019-06-14 17:39:08 +0100

    measurement_to_scores always returns vector

--- tests/testthat/test-functions.R
+++ tests/testthat/test-functions.R
@@ -2,5 +2,5 @@ context("test-functions")
 
 test_that("get_height_sds ok", {
   lms_stats <- .measurement_to_scores(4, 2, 'ht', 101.54)
-  expect_equal(lms_stats$z, 0)
+  expect_equal(lms_stats$z[1], 0)
 })
