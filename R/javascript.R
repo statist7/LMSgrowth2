cc9168edae7737f9290cc7f882457a27f09bae70
Asif Tamuri <tamuri@gmail.com>
2019-08-28 14:23:22 +0100

    only apply saved cookie state onload, prevent loops

--- R/javascript.R
+++ R/javascript.R
@@ -9,24 +9,24 @@
   // NOTE: namespace prefix "globals-" targets inputs in the globals module
   
   shinyjs.getcookies = function(params) {
-    Shiny.setInputValue("globals-jscookie", Cookies.get(), {priority: "event"});
+    Shiny.setInputValue("globals-jscookie", Cookies.get());
   }
 
   shinyjs.setcookie = function(params) {
     Cookies.set(params.name, escape(params.value), { expires: 365 });  
-    Shiny.setInputValue("globals-jscookie", Cookies.get(), {priority: "event"});
+    Shiny.setInputValue("globals-jscookie", Cookies.get());
   }
 
   shinyjs.rmcookie = function(name) {
     Cookies.remove(name);
-    Shiny.setInputValue("globals-jscookie", Cookies.get(), {priority: "event"});
+    Shiny.setInputValue("globals-jscookie", Cookies.get());
   }
   
   shinyjs.rmcookies = function(params) {
     for (var key in Cookies.get()) {
       Cookies.remove(key);
     }
-    Shiny.setInputValue("globals-jscookie", Cookies.get(), {priority: "event"})
+    Shiny.setInputValue("globals-jscookie", Cookies.get())
   }
 '
 
