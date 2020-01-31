3cdfe6b2c8107137ef73dd797cc4146aa71dc023
Asif Tamuri <tamuri@gmail.com>
2020-01-31 23:03:16 +0000

    Allow upload of user-defined growth reference data (#12)
    * upload custom growth references as csv and persist
    
    * add uuid package and update to R 3.6.2
    
    * tidy up setting of globals
    
    * create directory to hold user uploads
    
    * added uk90 growth ref in resources for testing

--- R/globals.R
+++ R/globals.R
@@ -8,16 +8,22 @@ source('R/functions.R', local = TRUE)
 
 .globalsUI <- function(id, label="globals ui") {
   ns <- NS(id)
-
+  
   fluidPage(
-    selectInput(ns("growth_ref"), label = "Growth reference", choices = .get_references())
+    selectInput(ns("growth_ref"), label = "Growth reference", choices=NULL),
+    fileInput(ns('growth_ref_file'), 'Upload growth reference file')
   )
 }
 
 .globals <- function(input, output, session) {
   # store the page load status
-  status <- reactiveValues(loaded=FALSE)
-
+  status <- reactiveValues(loaded=FALSE, uuid=NULL)
+  
+  # prefix for user uploads
+  USER_UPLOAD_DIR <- file.path(getwd(), 'uploads')
+  FILE_PREFIX <- '__LMSG2__'
+  GROWTH_REF_PREFIX <- paste0(FILE_PREFIX, 'GROWTH_REF__')
+  
   # object to store global values
   # globalValues$roundToDigits (numeric)
   #     - number of decimal digits for rounding
@@ -25,6 +31,8 @@ source('R/functions.R', local = TRUE)
   #     - number of significant digits for rounding
   # globalValues$growthReference (string)
   #     - the currently selected growth reference e.g. "uk90"
+  # globalValues$growthReferenceData (dataframe)
+  #     - the dataframe of currently selected growth reference
   # globalValues$growthReferenceName (string)
   #     - description of selected growth reference e.g. "UK 1990 growth reference"
   # globalValues$growthReferenceMeasures (character vector)
@@ -37,29 +45,101 @@ source('R/functions.R', local = TRUE)
   #     - the list of sexes available in currently selected growth reference
   globalValues <- reactiveValues(roundToDigits = 2,
                                  roundToSignificantDigits = 3)
-
-  # this block only executed once on page load to apply saved cookie state, if any
+  
+  # populates the growth reference select input using sitar data and any uploaded by user
+  update_growth_ref_input <- function(selected=NULL) {
+    # get the default growth references from sitar
+    all_gf <- .get_references()
+    
+    # check any growth references uploaded by the user
+    user_dir <- file.path(USER_UPLOAD_DIR, isolate(status$uuid))
+    if (dir.exists(user_dir)) {
+      user_files <- list.files(user_dir, pattern=paste0(GROWTH_REF_PREFIX, '*'))
+      user_gf_names <- str_match(user_files, paste0(GROWTH_REF_PREFIX, "(.*)"))
+      all_gf <- c(all_gf, setNames(as.list(user_files), user_gf_names[,2]))
+    }
+    
+    # if current selected has not been specified, pick it up from user's cookie, or pick first in list
+    if (is.null(selected)){
+      selected <- isolate(input$jscookie$growthRef)
+      if (!(selected %in% unlist(all_gf))) {
+        selected = all_gf[[1]]
+      }
+    }
+    
+    updateSelectInput(session, "growth_ref", choices = all_gf, selected = selected)
+  }
+  
   observe({
-    if (!status$loaded & !is.null(input$jscookie[["growthRef"]])) {
+    # this block only executed once on page load (status$loaded)
+    # and we wait for cookies to be initialised (input$jscookie)
+    if (!status$loaded & !is.null(input$jscookie)) {
+      # create user's uuid if one doesn't exist
+      uuid_cookie <- isolate(input$jscookie$uuid)
+      if (is.null(uuid_cookie)) {
+        new_uuid <- uuid::UUIDgenerate()
+        js$setcookie(name='uuid', value=new_uuid)
+        status$uuid <- new_uuid
+      } else {
+        status$uuid <- uuid_cookie
+      }
+      
+      # apply the user's saved growth reference, if any
+      growth_ref_cookie <- isolate(input$jscookie$growthRef)
+      if (is.null(growth_ref_cookie)) {
+        start_growth_ref <- 'uk90'
+      } else {
+        start_growth_ref <- growth_ref_cookie
+      }
+      
       status$loaded <- TRUE
-      saved_ref <- isolate(input$jscookie$growthRef)
-      updateSelectInput(session, "growth_ref", label="Growth reference", selected = saved_ref)
+      update_growth_ref_input(selected=start_growth_ref)
     }
   })
-
+  
   # if user selects growth_ref, then update the cookie
   observeEvent(input$growth_ref, {
-    js$setcookie(name='growthRef', value=input$growth_ref)
-    globalValues$growthReference <- input$growth_ref
-    references <- .get_references()
-    globalValues$growthReferenceName <- names(references)[references == globalValues$growthReference]
-    sitar_data <- .get_sitar_data(input$growth_ref)
-    globalValues$growthReferenceMeasures <- .get_measures_for_data(sitar_data)
-    ages <- .get_ages_for_data(sitar_data)
-    globalValues$growthReferenceAgeStart <- ages[1]
-    globalValues$growthReferenceAgeStop <- ages[length(ages)]
-    globalValues$growthReferenceSexes <- .get_sexes_for_data(sitar_data)
+    if (input$growth_ref != '') {
+      # we have a growth ref selected, make sure it persists across sessions
+      js$setcookie(name='growthRef', value=input$growth_ref)
+      
+      # if it's a user uploaded growth reference
+      if (stringr::str_starts(input$growth_ref, GROWTH_REF_PREFIX)) {
+        user_growth_ref <- file.path(USER_UPLOAD_DIR, input$jscookie$uuid, input$growth_ref)
+        growth_ref_data <- read.csv(user_growth_ref)
+        globalValues$growthReferenceName <- stringr::str_replace(input$growth_ref, GROWTH_REF_PREFIX, '')
+      } else {
+        # it's a sitar growth reference
+        references <- .get_references()
+        growth_ref_data <- .get_sitar_data(input$growth_ref)
+        globalValues$growthReferenceName <- names(references)[references == input$growth_ref]
+      }
+      
+      globalValues$growthReference <- input$growth_ref
+      globalValues$growthReferenceData <- growth_ref_data
+      globalValues$growthReferenceMeasures <- .get_measures_for_data(growth_ref_data)
+      globalValues$growthReferenceSexes <- .get_sexes_for_data(growth_ref_data)
+      
+      ages <- .get_ages_for_data(growth_ref_data)
+      globalValues$growthReferenceAgeStart <- ages[1]
+      globalValues$growthReferenceAgeStop <- ages[length(ages)]
+    }
   })
-
+  
+  # when use uploads a growth reference file
+  observeEvent(input$growth_ref_file, {
+    # TODO: validate input!
+    
+    # save uploaded growth file to user's directory, creating the directory if it doesn't exist
+    user_dir <- file.path(USER_UPLOAD_DIR, input$jscookie$uuid)
+    dir.create(user_dir)
+    target_filename <- paste0(GROWTH_REF_PREFIX, input$growth_ref_file$name)
+    target_path <- file.path(user_dir, target_filename)
+    file.copy(input$growth_ref_file$datapath, target_path)
+    
+    # add this growth references to available list
+    update_growth_ref_input(selected=target_filename)
+  })
+  
   return(globalValues)
 }
